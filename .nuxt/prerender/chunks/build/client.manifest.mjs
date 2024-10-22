daf2818a2f6fb63e54ad9431604b262fb2b38cd2
houider <houiderwalid@gmail.com>
2024-10-22 21:46:56 +0100

    small fix 2

---
+++ .nuxt/prerender/chunks/build/client.manifest.mjs
@@ -0,0 +1,240 @@
+const client_manifest = {
+  "_CdDAqJ1c.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CdDAqJ1c.js",
+    "name": "vue.65b7bcda",
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ]
+  },
+  "assets/font/death_note/death_note.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "death_note.DSxoIBRA.ttf",
+    "src": "assets/font/death_note/death_note.ttf"
+  },
+  "assets/font/nerko/nerko_one_regular.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "nerko_one_regular.CInRe7Ln.ttf",
+    "src": "assets/font/nerko/nerko_one_regular.ttf"
+  },
+  "assets/font/protest_guerrilla/protest_guerrilla_regular.ttf": {
+    "resourceType": "font",
+    "mimeType": "font/ttf",
+    "file": "protest_guerrilla_regular.DLF_AtFO.ttf",
+    "src": "assets/font/protest_guerrilla/protest_guerrilla_regular.ttf"
+  },
+  "layouts/default.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "CvxTkpdr.js",
+    "name": "default",
+    "src": "layouts/default.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "default.CJlT2mHE.css"
+    ],
+    "assets": [
+      "intro-bg.DCHHKMCM.jpg",
+      "43239991.DkYmBkun.png",
+      "simo_travel.Bh7VMY2h.png",
+      "dash.maxcom.network.BVKA8a_z.png",
+      "maxcom.network.Dw1S95ng.png"
+    ]
+  },
+  "default.CJlT2mHE.css": {
+    "file": "default.CJlT2mHE.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "intro-bg.DCHHKMCM.jpg": {
+    "file": "intro-bg.DCHHKMCM.jpg",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg"
+  },
+  "43239991.DkYmBkun.png": {
+    "file": "43239991.DkYmBkun.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "simo_travel.Bh7VMY2h.png": {
+    "file": "simo_travel.Bh7VMY2h.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "dash.maxcom.network.BVKA8a_z.png": {
+    "file": "dash.maxcom.network.BVKA8a_z.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "maxcom.network.Dw1S95ng.png": {
+    "file": "maxcom.network.Dw1S95ng.png",
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png"
+  },
+  "node_modules/nuxt/dist/app/components/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "COl6Fj-B.js",
+    "name": "error-404",
+    "src": "node_modules/nuxt/dist/app/components/error-404.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js",
+      "_CdDAqJ1c.js"
+    ],
+    "css": [
+      "error-404.DXySnQZL.css"
+    ]
+  },
+  "error-404.DXySnQZL.css": {
+    "file": "error-404.DXySnQZL.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/components/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "DQWu89PW.js",
+    "name": "error-500",
+    "src": "node_modules/nuxt/dist/app/components/error-500.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "_CdDAqJ1c.js",
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "error-500.CIkJDsQj.css"
+    ]
+  },
+  "error-500.CIkJDsQj.css": {
+    "file": "error-500.CIkJDsQj.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "node_modules/nuxt/dist/app/entry.js": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "Cz_YN9Mi.js",
+    "name": "entry",
+    "src": "node_modules/nuxt/dist/app/entry.js",
+    "isEntry": true,
+    "dynamicImports": [
+      "layouts/default.vue",
+      "node_modules/nuxt/dist/app/components/error-404.vue",
+      "node_modules/nuxt/dist/app/components/error-500.vue"
+    ],
+    "css": [
+      "entry.rZ7HzOyq.css"
+    ],
+    "assets": [
+      "death_note.DSxoIBRA.ttf",
+      "nerko_one_regular.CInRe7Ln.ttf",
+      "protest_guerrilla_regular.DLF_AtFO.ttf"
+    ]
+  },
+  "entry.rZ7HzOyq.css": {
+    "file": "entry.rZ7HzOyq.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "death_note.DSxoIBRA.ttf": {
+    "file": "death_note.DSxoIBRA.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "nerko_one_regular.CInRe7Ln.ttf": {
+    "file": "nerko_one_regular.CInRe7Ln.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "protest_guerrilla_regular.DLF_AtFO.ttf": {
+    "file": "protest_guerrilla_regular.DLF_AtFO.ttf",
+    "resourceType": "font",
+    "mimeType": "font/ttf"
+  },
+  "pages/index.vue": {
+    "resourceType": "script",
+    "module": true,
+    "prefetch": true,
+    "preload": true,
+    "file": "PgCEDTPz.js",
+    "name": "index",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.js"
+    ],
+    "css": [
+      "index.l9CJHO8v.css"
+    ]
+  },
+  "index.l9CJHO8v.css": {
+    "file": "index.l9CJHO8v.css",
+    "resourceType": "style",
+    "prefetch": true,
+    "preload": true
+  },
+  "public/43239991.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "43239991.DkYmBkun.png",
+    "src": "public/43239991.png"
+  },
+  "public/dash.maxcom.network.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "dash.maxcom.network.BVKA8a_z.png",
+    "src": "public/dash.maxcom.network.png"
+  },
+  "public/intro-bg.jpg": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/jpeg",
+    "file": "intro-bg.DCHHKMCM.jpg",
+    "src": "public/intro-bg.jpg"
+  },
+  "public/maxcom.network.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "maxcom.network.Dw1S95ng.png",
+    "src": "public/maxcom.network.png"
+  },
+  "public/simo_travel.png": {
+    "resourceType": "image",
+    "prefetch": true,
+    "mimeType": "image/png",
+    "file": "simo_travel.Bh7VMY2h.png",
+    "src": "public/simo_travel.png"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
